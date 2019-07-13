709a225468004b2c26dce4fad5085e005efedf55
Helga Pavluchenko <olga@pavluchenko.com>
2019-07-13 20:15:50 +0300

    initial commit

---
+++ opensbubble/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  opensbubble
+//
+//  Created by Helga on 7/13/19.
+//  Copyright © 2019 Helga. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
